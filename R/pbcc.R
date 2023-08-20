b73e56cf316df8476b5d22975a4c3d48e6867dc8
Zsolt T. Kosztyan <kosztyan.zsolt@gtk.uni-pannon.hu>
2023-08-20 22:40:02 +0000

    version 0.0.4

--- R/pbcc.R
+++ R/pbcc.R
@@ -11,7 +11,9 @@
 #-----------------------------------------------------------------------------#
 
 #' @export
-pbcc<- function(data,T1, p1, type=c("Xbar", "R", "S", "S2","Xbar-R","Xbar-S","Xbar-S2"), sided="two")
+pbcc<- function(data,T1, p1, type=c("Xbar", "R", "S", "S2",
+                                    "Xbar-R","Xbar-S","Xbar-S2"), sided="two",
+                k=NULL, lp=NULL, up=NULL)
 {
   if (!requireNamespace("qcc", quietly = TRUE)) {
     stop(
@@ -23,26 +25,30 @@ pbcc<- function(data,T1, p1, type=c("Xbar", "R", "S", "S2","Xbar-R","Xbar-S","Xb
       call. = FALSE ) }
 
   call <- match.call()
-  if (missing(data))
+  if (is.null(data))
     stop("'data' argument is not specified")
   if(identical(type, eval(formals(pbcc)$type)))
   { type <- as.character(type)[1]
   warning("chart 'type' not specified, assuming \"", type, "\"",
           immediate. = TRUE) }
-  if(missing(p1))
+  if(is.null(p1))
   {p1 <- 0.05}
-  if(missing(T1))
+  if(is.null(T1))
   {T1 <- 100}
-  if(missing(sided))
+  if(is.null(sided))
   {sided <- "two"}
   data.name <- deparse(substitute(data))
   sizes <- as.numeric(ncol(data))
   alfa <- 1-exp(log(1-p1)/T1)
   alpha <- alfa
+  alpha1 <- 1-sqrt(1-alfa)
 if("Xbar" %in% type )
  {
 
-  k <- stats::qnorm(1-alpha/2)
+  if(is.null(k))
+  {
+    k <- stats::qnorm(1-alpha/2)
+  }
   q1 <- qcc::qcc(data, type="xbar", plot=FALSE)
   Statisticsx <- q1$statistics
   std.dev <- q1$std.dev
@@ -54,19 +60,27 @@ if("Xbar" %in% type )
   }
 if("R" %in% type )
   {
+
   if (sided=="one"){
-    lu <- stats::qtukey(alpha, sizes, Inf, lower.tail=FALSE)
-    ll <- 0
+    if(is.null(lp))
+    {lp <- 0}
+    if(is.null(up))
+    {up <- stats::qtukey(alpha, sizes, Inf, lower.tail=FALSE)}
+
   }else{
-    ll <- stats::qtukey(alpha/2, sizes, Inf)
-    lu <- stats::qtukey(alpha/2, sizes, Inf, lower.tail=FALSE)
+
+    if(is.null(lp))
+    {lp <- stats::qtukey(alpha/2, sizes, Inf)}
+    if(is.null(up))
+    {up <- stats::qtukey(alpha/2, sizes, Inf, lower.tail=FALSE)}
+
   }
     q1 <- qcc::qcc(data, type="R", plot=FALSE)
     std.dev <- q1$std.dev
     StatisticsR <- q1$statistics
-    lclR <- ll*q1$center/qcc::qcc.options("exp.R.unscaled")[sizes]
+    lclR <- lp*q1$center/qcc::qcc.options("exp.R.unscaled")[sizes]
     clR <-  q1$center
-    uclR <- lu*q1$center/qcc::qcc.options("exp.R.unscaled")[sizes]
+    uclR <- up*q1$center/qcc::qcc.options("exp.R.unscaled")[sizes]
     output <- list(data.name=data.name,type=type,sizes=sizes, std.dev=std.dev, statistics=StatisticsR, LCL=lclR, CL=clR, UCL=uclR)
 }
   if("S" %in% type )
@@ -74,29 +88,40 @@ if("R" %in% type )
     qcc.c4 <- function(n)
     { sqrt(2/(n - 1)) * exp(lgamma(n/2) - lgamma((n - 1)/2)) }
     if (sided=="one"){
-      lu <- sqrt(stats::qchisq(1-alpha, sizes-1))
-      ll <- 0
+      if(is.null(lp))
+      {lp <- 0}
+     if(is.null(up))
+      { up <- sqrt(stats::qchisq(1-alpha, sizes-1))}
+
     }else{
-      ll <- sqrt(stats::qchisq(alpha/2, sizes-1))
-      lu <- sqrt(stats::qchisq(1-alpha/2, sizes-1))
+
+      if(is.null(lp))
+      {lp <- sqrt(stats::qchisq(alpha/2, sizes-1))}
+      if(is.null(up))
+      { up <- sqrt(stats::qchisq(1-alpha/2, sizes-1))}
     }
     q1 <- qcc::qcc(data, type="S", plot=FALSE)
     std.dev <- q1$std.dev
     StatisticsS <- q1$statistics
-    lclS <- ll*q1$center/qcc.c4(sizes)
+    lclS <- (lp/sqrt(sizes-1))*q1$center/qcc.c4(sizes)
     clS <- q1$center
-    uclS <- lu*q1$center/qcc.c4(sizes)
+    uclS <- (up/sqrt(sizes-1))*q1$center/qcc.c4(sizes)
     output <- list(data.name=data.name,type=type, sizes=sizes, std.dev=std.dev, statistics=StatisticsS, LCL=lclS, CL=clS, UCL=uclS)
   }
   if("S2" %in% type )
   {
 
-        if (sided=="one"){
-      lu <- stats::qchisq(1-alpha, sizes-1)
-      ll <- 0
+    if (sided=="one"){
+
+          if(is.null(lp))
+          {lp <- 0}
+          if(is.null(up))
+          { up <- stats::qchisq(1-alpha, sizes-1)}
     }else{
-      ll <- stats::qchisq(alpha/2, sizes-1)
-      lu <- stats::qchisq(1-alpha/2, sizes-1)
+      if(is.null(lp))
+      {lp <- stats::qchisq(alpha/2, sizes-1)}
+      if(is.null(up))
+      { up <- stats::qchisq(1-alpha/2, sizes-1)}
     }
 
     data <- as.matrix(data)
@@ -106,20 +131,28 @@ if("R" %in% type )
     center <- sum(sizes1 * statistics)/sum(sizes1)
     std.dev <- sqrt(2/(sizes-1))* (sum(statistics)/length(sizes1))
     StatisticsS2 <- statistics
-    lclS2 <- ll*center/(sizes-1)
+    lclS2 <- lp*center/(sizes-1)
     clS2 <- center
-    uclS2 <- lu*center/(sizes-1)
+    uclS2 <- up*center/(sizes-1)
     output <- list(data.name=data.name,type=type,sizes=sizes, std.dev=std.dev, statistics=StatisticsS2, LCL=lclS2, CL=clS2, UCL=uclS2)
   }
 if("Xbar-R" %in% type )
   {
-    k <- stats::qnorm(1-alpha/2)
+
+  if(is.null(k))
+  {k <- stats::qnorm(1-alpha1/2)}
+
   if (sided=="one") {
-      lu <- stats::qtukey(alpha, sizes, Inf, lower.tail=FALSE)
-      ll <- 0
+
+    if(is.null(lp))
+    {lp <- 0}
+    if(is.null(up))
+    {up <- stats::qtukey(alpha1, sizes, Inf, lower.tail=FALSE)}
     }else {
-      ll <- stats::qtukey(alpha/2, sizes, Inf)
-      lu <- stats::qtukey(alpha/2, sizes, Inf, lower.tail=FALSE)
+      if(is.null(lp))
+      {lp <- stats::qtukey(alpha1/2, sizes, Inf)}
+      if(is.null(up))
+      {up <- stats::qtukey(alpha1/2, sizes, Inf, lower.tail=FALSE)}
     }
     q1 <- qcc::qcc(data, type="xbar", plot=FALSE)
     std.devx <- q1$std.dev
@@ -130,23 +163,31 @@ if("Xbar-R" %in% type )
     q2 <- qcc::qcc(data, type="R", plot=FALSE)
     std.devR <- q2$std.dev
     StatisticsR <- q2$statistics
-    lclR <- ll*q2$center/qcc::qcc.options("exp.R.unscaled")[sizes]
+    lclR <- lp*q2$center/qcc::qcc.options("exp.R.unscaled")[sizes]
     clR <- q2$center
-    uclR <- lu*q2$center/qcc::qcc.options("exp.R.unscaled")[sizes]
+    uclR <- up*q2$center/qcc::qcc.options("exp.R.unscaled")[sizes]
     output <- list(data.name=data.name,type=type, sizes=sizes,std.dev=std.devx, statistics=Statisticsx,LCL=lclx, CL=clx, UCL=uclx, std.dev1=std.devR, statistics1=StatisticsR,LCL1=lclR, CL1=clR, UCL1=uclR)
   }
 
   if("Xbar-S" %in% type )
   {
-    k <- stats::qnorm(1-alpha/2)
+
+    if(is.null(k))
+    {k <- stats::qnorm(1-alpha1/2)}
     qcc.c4 <- function(n)
     { sqrt(2/(n - 1)) * exp(lgamma(n/2) - lgamma((n - 1)/2)) }
     if (sided=="one"){
-      lu <- sqrt(stats::qchisq(1-alpha, sizes-1))
-      ll <- 0
+
+      if(is.null(lp))
+      {lp <- 0}
+      if(is.null(up))
+      {up <- sqrt(stats::qchisq(1-alpha1, sizes-1))}
+
     }else{
-      ll <- sqrt(stats::qchisq(alpha/2, sizes-1))
-      lu <- sqrt(stats::qchisq(1-alpha/2, sizes-1))
+      if(is.null(lp))
+      {lp <- sqrt(stats::qchisq(alpha1/2, sizes-1))}
+      if(is.null(up))
+      {up <- sqrt(stats::qchisq(1-alpha1/2, sizes-1))}
     }
     q1 <- qcc::qcc(data, type="xbar", plot=FALSE)
     std.devx <- q1$std.dev
@@ -157,21 +198,28 @@ if("Xbar-R" %in% type )
     q2 <- qcc::qcc(data, type="S", plot=FALSE)
     std.devS <- q2$std.dev
     StatisticsS <- q2$statistics
-    lclS <- ll*q2$center/qcc.c4(sizes)
+    lclS <- (lp/sqrt(sizes-1))*q2$center/qcc.c4(sizes)
     clS <- q2$center
-    uclS <- lu*q2$center/qcc.c4(sizes)
+    uclS <- (up/sqrt(sizes-1))*q2$center/qcc.c4(sizes)
     output <- list(data.name=data.name,type=type,sizes=sizes,std.dev=std.devx,statistics=Statisticsx,LCL=lclx, CL=clx, UCL=uclx, std.dev1=std.devS, statistics1=StatisticsS,LCL1=lclS, CL1=clS, UCL1=uclS)
   }
 
   if("Xbar-S2" %in% type )
   {
-    k <- stats::qnorm(1-alpha/2)
+    if(is.null(k))
+    {k <- stats::qnorm(1-alpha1/2)}
+
     if (sided=="one"){
-      lu <- stats::qchisq(1-alpha, sizes-1)
-      ll <- 0
+      if(is.null(lp))
+      {lp <- 0}
+      if(is.null(up))
+      {up <- stats::qchisq(1-alpha1, sizes-1)}
+
     }else{
-      ll <- stats::qchisq(alpha/2, sizes-1)
-      lu <- stats::qchisq(1-alpha/2, sizes-1)
+      if(is.null(lp))
+      {lp <- stats::qchisq(alpha1/2, sizes-1)}
+      if(is.null(up))
+      {up <- stats::qchisq(1-alpha1, sizes-1)}
     }
     q1 <- qcc::qcc(data, type="xbar", plot=FALSE)
     std.devx <- q1$std.dev
@@ -186,9 +234,9 @@ if("Xbar-R" %in% type )
     centerS2 <- sum(sizes1 * statistics)/sum(sizes1)
     std.devS2 <- sqrt(2/(sizes-1))* (sum(statistics)/length(sizes1))
     StatisticsS2 <- statistics
-    lclS2 <- ll*centerS2/(sizes-1)
+    lclS2 <- lp*centerS2/(sizes-1)
     clS2 <- centerS2
-    uclS2 <- lu*centerS2/(sizes-1)
+    uclS2 <- up*centerS2/(sizes-1)
     output <- list(data.name=data.name,type=type, sizes=sizes,std.dev=std.devx,statistics=Statisticsx,LCL=lclx, CL=clx, UCL=uclx, std.dev1=std.devS2,statistics1=StatisticsS2,LCL1=lclS2, CL1=clS2, UCL1=uclS2)
   }
   class(output) <- "pbcc"
